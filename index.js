b64f66a97b2c44531654100ec131a7173b8ce307
Arablee <arablee201@gmail.com>
2022-05-04 16:51:23 +0400

    work

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const db = require("./DbConfig/DbConnection")
+const userAPI = require("./controllers/user")
 const express = require('express');
 const app = express();
 
+
 const PORT = process.env.PORT;
+
+
+app.use("/", userAPI);
+
 
 db().then(()=>{
     app.listen(PORT,()=>{
